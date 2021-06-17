56773e23a2ca70790f159848899d17026486f3f3
akshay-raj-dhamija <akshay.raj.dhamija@gmail.com>
2021-06-17 14:49:31 -0600

    Initial Feature Distribution Commit

---
+++ Feature_Distribution/Feature_Distribution.py
@@ -0,0 +1,116 @@
+import argparse
+import numpy as np
+import pathlib
+import itertools
+import torch
+torch.multiprocessing.set_sharing_strategy('file_system')
+import torch.multiprocessing as mp
+import common_operations
+from vast import opensetAlgos
+from vast.tools import logger as vastlogger
+from vast.data_prep import readHDF5
+
+def command_line_options():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                     add_help=False, usage=argparse.SUPPRESS)
+
+    parser.add_argument('-v', '--verbose', help="To decrease verbosity increase", action='count', default=0)
+    parser.add_argument("--debug", action="store_true", default=False, help="debugging flag")
+    parser.add_argument("--no_multiprocessing", action="store_true", default=False,
+                        help="Use for debugging or running on single GPU")
+    parser.add_argument('--port_no', default='9451', type=str,
+                        help='port number for multiprocessing\ndefault: %(default)s')
+    parser.add_argument("--output_dir", type=str, default='/scratch/adhamija/results/', help="Results directory")
+    parser.add_argument('--OOD_Algo', default='EVM', type=str, choices=['OpenMax','EVM','Turbo_EVM','MultiModalOpenMax'],
+                        help='Name of the openset detection algorithm')
+
+    parser.add_argument("--training_knowns_files", nargs="+", help="HDF5 file path for known images",
+                        default=["/net/reddwarf/bigscratch/adhamija/Features/MOCOv2/imagenet_1000_val.hdf5"])
+    parser.add_argument("--training_unknowns_files", nargs="+", help="HDF5 file path for unknown images",
+                        default=None)
+    parser.add_argument("--testing_files", nargs="+", help="HDF5 file path for known images",
+                        default=["/net/reddwarf/bigscratch/adhamija/Features/MOCOv2/imagenet_1000_val.hdf5"])
+    parser.add_argument("--layer_names", nargs="+", help="The layers to extract from each file", default=["avgpool"])
+
+    known_args, unknown_args = parser.parse_known_args()
+
+    # Adding Algorithm Params
+    params_parser = argparse.ArgumentParser(parents = [parser],formatter_class = argparse.RawTextHelpFormatter,
+                                            usage=argparse.SUPPRESS,
+                                            description = "This script runs experiments for incremental learning " 
+                                                          "i.e. Table 1 and 2 from the Paper")
+    parser, algo_params = getattr(opensetAlgos, known_args.OOD_Algo + '_Params')(params_parser)
+    args = parser.parse_args()
+    return args, algo_params
+
+if __name__ == "__main__":
+    mp.set_start_method('forkserver', force=True)
+    args, algo_params = command_line_options()
+    args.output_dir = pathlib.Path(f"{args.output_dir}/{'_'.join(args.layer_names)}/{args.OOD_Algo}/")
+    logger = vastlogger.setup_logger(level=args.verbose, output=args.output_dir)
+
+    args.world_size = torch.cuda.device_count()
+    if args.world_size==1:
+        args.no_multiprocessing = True
+    if args.debug:
+        args.verbose = 0
+
+    # all_string_comb = [algo_params['param_id_string'].format(*k).replace('.','*') for k in
+    #                    itertools.product(*[args.__dict__[a] for a in algo_params['param_names']])]
+    all_string_comb = [algo_params['param_id_string'].format(*k) for k in
+                       itertools.product(*[args.__dict__[a] for a in algo_params['param_names']])]
+    args.param_comb_to_saver_mapping = dict(zip(range(args.world_size, len(all_string_comb) + args.world_size),
+                                                all_string_comb))
+    ranks, combs = zip(*args.param_comb_to_saver_mapping.items())
+    args.param_comb_to_saver_mapping_reverse = dict(zip(combs, ranks))
+
+    args.feature_files = args.training_knowns_files
+    training_data = readHDF5.prep_all_features_parallel(args)
+    training_data = dict([(_, training_data[_]['features']) for _ in training_data])
+
+    event = mp.Event()
+    trainer_processes = mp.spawn(common_operations.call_specific_approach,
+                                 args=(args, training_data, event),
+                                 nprocs=args.world_size,
+                                 join=False)
+    processes = []
+    for rank in range(args.world_size,args.world_size+len(args.param_comb_to_saver_mapping)):
+        p = mp.Process(target=common_operations.saver_process_initialization,
+                       args=(rank, args, len(training_data), args.param_comb_to_saver_mapping, None))
+        p.start()
+        processes.append(p)
+
+    for p in processes: p.join()
+    trainer_processes.join()
+    # Use for testing
+    # Load the test feature files
+    all_testing_data=[]
+    for testing_file in args.testing_files:
+        args.feature_files = [testing_file]
+        testing_data = readHDF5.prep_all_features_parallel(args)
+        testing_data = dict([(_, testing_data[_]['features']) for _ in testing_data])
+        all_testing_data.append([testing_file, testing_data])
+
+    if args.OOD_Algo not in ['EVM','Turbo_EVM']: training_data=None
+    # Load the model
+    for grid_serach_param_combination in args.param_comb_to_saver_mapping.values():
+        OOD_model_dict =  opensetAlgos.save_load_operations.model_loader(args,
+                                                                         grid_serach_param_combination,
+                                                                         training_data)
+        for testing_file, testing_data in all_testing_data:
+            output_file_name = args.output_dir/pathlib.Path(f"{grid_serach_param_combination}/"
+                                                            f"{testing_file.split('/')[-2]}")
+            output_file_name.mkdir(parents=True, exist_ok=True)
+            output_file_name = output_file_name/pathlib.Path(f"{testing_file.split('/')[-1]}")
+            event.clear()
+            testing_processes = mp.spawn(common_operations.call_specific_approach,
+                                         args=(args, testing_data, event, OOD_model_dict),
+                                         nprocs=args.world_size,
+                                         join=False)
+            saver_process = mp.Process(target=common_operations.saver_process_initialization,
+                                       args=(args.world_size, args, len(testing_data),
+                                             {args.world_size:testing_file},
+                                             output_file_name))
+            saver_process.start()
+            saver_process.join()
+            testing_processes.join()
---
+++ Feature_Distribution/common_operations.py
@@ -0,0 +1,144 @@
+import os
+import h5py
+import vast
+import torch
+import torch.distributed.rpc as rpc
+from vast.tools import logger as vastlogger
+from vast.opensetAlgos import save_load_operations as save_load_operations
+from typing import Generator, List, Dict, Tuple
+
+model_saver_obj = None
+class save_inference_results:
+    def __init__(self, args,
+                 process_combination: Tuple[str, int],
+                 total_no_of_classes: int,
+                 output_file_name: str = None) -> None:
+        self.total_no_of_classes = total_no_of_classes
+        self.processed_classes = 0
+        self.hf = h5py.File(output_file_name, "w")
+
+    def __call__(self, cls_name, data):
+        layer_name, values = data
+        g = self.hf.create_group(cls_name)
+        g.create_dataset(layer_name, data=values)
+        self.processed_classes+=1
+
+    def wait(self):
+        while True:
+            if self.processed_classes >= self.total_no_of_classes:
+                break
+        return
+
+    def close(self):
+        self.hf.close()
+
+def saver_process_execution(cls_name, data):
+    model_saver_obj(cls_name, data)
+
+def saver_process_initialization(rank, args, total_no_of_classes = None, savers_mapping = None, output_file_name=None):
+    global model_saver_obj
+    world_size = args.world_size + len(savers_mapping)
+    if args.world_size>1:
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = args.port_no
+        rpc.init_rpc(f"{rank}",
+                     rank=rank,
+                     world_size = world_size,
+                     backend=rpc.BackendType.TENSORPIPE,
+                     rpc_backend_options=rpc.TensorPipeRpcBackendOptions(#num_send_recv_threads=args.world_size*3,
+                                                                         rpc_timeout=0,
+                                                                         init_method='env://')
+                     )
+        logger = vastlogger.get_logger(level=args.verbose, output=args.output_dir,
+                                        distributed_rank=rank, world_size=world_size)
+        logger.info(f"Started RPC Saver process")
+    else:
+        logger = vastlogger.get_logger()
+    if output_file_name is None:
+        logger.debug(f"Calling model saver")
+        saver_class = save_load_operations.model_saver
+    else:
+        logger.debug(f"Calling inference file saver")
+        saver_class = save_inference_results
+    model_saver_obj = saver_class(args,
+                                  (savers_mapping[rank], rank), #args.param_comb_to_saver_mapping
+                                  total_no_of_classes,
+                                  output_file_name)
+    model_saver_obj.wait()
+    model_saver_obj.close()
+    logger.info(f"Shutting down RPC saver process for combination {savers_mapping[rank]}")
+    rpc.shutdown()
+    return
+
+def call_distance_based_approaches(gpu, args, features_all_classes, logger, models,
+                                   new_classes_to_add = None, event=None):
+    if new_classes_to_add is None:
+        class_names = list(features_all_classes.keys())
+        exemplar_classes = []
+        for _ in class_names:
+            if 'exemplars_' in _:
+                exemplar_classes.append(_)
+        if len(exemplar_classes):
+            logger.info(" Removing Exemplars from positive classes to be processed ".center(90, '#'))
+        class_names = sorted(list(set(class_names)-set(exemplar_classes)))
+    else:
+        class_names = new_classes_to_add
+    div, mod = divmod(len(class_names), args.world_size)
+    pos_classes_to_process = class_names[gpu * div + min(gpu, mod):(gpu + 1) * div + min(gpu + 1, mod)]
+
+    # pos_classes_to_process = pos_classes_to_process[:2]
+    logger.debug(f"Processing classes {pos_classes_to_process}")
+
+    if models is None:
+        OOD_Method = getattr(vast.opensetAlgos, args.OOD_Algo + '_Training')
+        logger.info(f"Calling approach {OOD_Method.__name__}")
+        algorithm_results_iterator = OOD_Method(pos_classes_to_process, features_all_classes, args, gpu, models)
+        for current_class_output in algorithm_results_iterator:
+            param_comb, (cls_name, model) = current_class_output
+            model['weibulls'] = model['weibulls'].return_all_parameters()
+            if 'extreme_vectors' in model:
+                del model['extreme_vectors']
+
+            _ = rpc.remote(f"{args.param_comb_to_saver_mapping_reverse[param_comb]}",
+                           saver_process_execution,
+                           timeout=0,
+                           args=(cls_name, model))
+    else:
+        OOD_Method = getattr(vast.opensetAlgos, args.OOD_Algo + '_Inference')
+        logger.info(f"Calling approach {OOD_Method.__name__}")
+        algorithm_results_iterator = OOD_Method(pos_classes_to_process, features_all_classes, args, gpu, models)
+        for current_class_output in algorithm_results_iterator:
+            layer_name, (cls_name, probs) = current_class_output
+            _ = rpc.remote(f"{args.world_size}",
+                           saver_process_execution,
+                           timeout=0,
+                           args=(cls_name, (layer_name, probs)))
+    logger.debug(f"Shutting down")
+    rpc.shutdown()
+    return
+
+def call_specific_approach(rank, args, features_all_classes, event=None,
+                           models=None, new_classes_to_add = None):
+    if args.world_size>1:
+        if models is None:
+            world_size = args.world_size + len(args.param_comb_to_saver_mapping)
+        else:
+            world_size = args.world_size + 1
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = args.port_no
+        rpc.init_rpc(f"{rank}",
+                     rank=rank,
+                     world_size=world_size,
+                     backend=rpc.BackendType.TENSORPIPE,
+                     rpc_backend_options=rpc.TensorPipeRpcBackendOptions(#num_send_recv_threads=args.world_size*3,
+                                                                         rpc_timeout=0)
+                     )
+        torch.cuda.set_device(rank)
+        os.environ["CUDA_VISIBLE_DEVICES"] = f"{rank}"
+        logger = vastlogger.get_logger(level=args.verbose, output=args.output_dir,
+                                        distributed_rank=rank, world_size=world_size)
+        logger.debug(f"Started process")
+    else:
+        logger = vastlogger.get_logger()
+    return call_distance_based_approaches(rank, args, features_all_classes, logger,
+                                          models, new_classes_to_add, event)
